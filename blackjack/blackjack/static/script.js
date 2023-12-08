472f0a97d8d909701cfb014a3f2d712623e39521
BigNoahW <149721935+BigNoahW@users.noreply.github.com>
2023-12-08 10:56:47 -0500

    Update script.js

--- blackjack/blackjack/static/script.js
+++ blackjack/blackjack/static/script.js
@@ -1,1 +1,40 @@
+document.addEventListener('DOMContentLoaded', function () {
+    updateState();
 
+    document.getElementById('hit-btn').addEventListener('click', function () {
+        fetch('/hit')
+            .then(response => response.json())
+            .then(data => {
+                updateState();
+                alert(data.message);
+            });
+    });
+
+    document.getElementById('stand-btn').addEventListener('click', function () {
+        fetch('/stand')
+            .then(response => response.json())
+            .then(data => {
+                updateState();
+                alert(data.message);
+            });
+    });
+
+    document.getElementById('deal-btn').addEventListener('click', function () {
+        fetch('/deal')
+            .then(response => response.json())
+            .then(data => {
+                updateState();
+                alert('New round started.');
+            });
+    });
+
+    function updateState() {
+        fetch('/get_state')
+            .then(response => response.json())
+            .then(data => {
+                document.getElementById('player-hand').innerText = 'Player Hand: ' + data.player_hand.join(', ');
+                document.getElementById('dealer-hand').innerText = 'Dealer Hand: ' + data.dealer_hand.join(', ');
+                document.getElementById('result').innerText = 'Result: ' + data.result;
+            });
+    }
+});
