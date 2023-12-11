040d1ab91149200c53d9683a63453935b220d2e4
BigNoahW <149721935+BigNoahW@users.noreply.github.com>
2023-12-11 01:00:50 +0000

    biqep

--- file/static/main.js
+++ file/static/main.js
@@ -34,14 +34,31 @@ function getDeck() {
     return deck;
 }
     // Implement logic to generate a deck of cards
-}
+
 
 // Function to shuffle the deck
 function shuffleDeck(deck) {
-    // Implement logic to shuffle the deck
+    for (let i = deck.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        // Swap deck[i] and deck[j]
+        [deck[i], deck[j]] = [deck[j], deck[i]];
+    }
 }
+    // Implement logic to shuffle the deck
+
+    //let deck = getDeck();
+    //shuffleDeck(deck);
+
 
 // Function to draw a card from the deck
 function drawCard() {
+    if (deck.length === 0) {
+        // If the deck is empty, create a new one and shuffle
+        deck = getDeck();
+        shuffleDeck(deck);
+    }
+    
+    // Draw the top card from the deck
+    return deck.pop();
     // Implement logic to draw a card from the deck
 }
