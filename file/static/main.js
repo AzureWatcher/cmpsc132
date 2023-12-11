d842b1ce3f4a1a24d95584e18754c39410850a9c
BigNoahW <149721935+BigNoahW@users.noreply.github.com>
2023-12-11 22:55:44 +0000

    dealer

--- file/static/main.js
+++ file/static/main.js
@@ -64,7 +64,121 @@ $(document).ready(function() {
 
     $('#stand-btn').click(function() {
         // Implement dealer logic here (draw until 17)
+        
         // Update the UI accordingly
         // Determine the winner
     });
+    // static/main.js
+$(document).ready(function() {
+    let deck = [];
+    let playerHand = [];
+    let dealerHand = [];
+
+    function getDeck() {
+        // ... (same as before)
+    }
+
+    function shuffleDeck(deck) {
+        // ... (same as before)
+    }
+
+    function drawCard() {
+        // ... (same as before)
+    }
+
+    function startGame() {
+        playerHand = [drawCard(), drawCard()];
+        dealerHand = [drawCard(), drawCard()];
+
+        updateHands();
+        $('#start-btn').prop('disabled', true);
+        $('#hit-btn').prop('disabled', false);
+        $('#stand-btn').prop('disabled', false);
+    }
+
+    function updateHands() {
+        // ... (same as before)
+    }
+
+    function formatHand(hand) {
+        // ... (same as before)
+    }
+
+    function calculateHandTotal(hand) {
+        let total = 0;
+        let hasAce = false;
+
+        for (const card of hand) {
+            const value = card.rank === 'Ace' ? 11 : (isNaN(card.rank) ? 10 : parseInt(card.rank));
+            total += value;
+
+            if (card.rank === 'Ace') {
+                hasAce = true;
+            }
+        }
+
+        // Adjust for Ace if necessary
+        if (hasAce && total > 21) {
+            total -= 10;
+        }
+
+        return total;
+    }
+
+    function playDealer() {
+        while (calculateHandTotal(dealerHand) < 17) {
+            dealerHand.push(drawCard());
+        }
+
+        updateHands();
+
+        // Determine the winner and end the game
+        determineWinner();
+    }
+
+    function determineWinner() {
+        const playerTotal = calculateHandTotal(playerHand);
+        const dealerTotal = calculateHandTotal(dealerHand);
+
+        if (playerTotal > 21 || (dealerTotal <= 21 && dealerTotal >= playerTotal)) {
+            // Dealer wins
+            alert('Dealer Wins!');
+        } else {
+            // Player wins
+            alert('Player Wins!');
+        }
+
+        // Reset the game
+        $('#start-btn').prop('disabled', false);
+        $('#hit-btn').prop('disabled', true);
+        $('#stand-btn').prop('disabled', true);
+    }
+
+    $('#start-btn').click(function() {
+        startGame();
+    });
+
+    $('#hit-btn').click(function() {
+        playerHand.push(drawCard());
+        updateHands();
+
+        const playerTotal = calculateHandTotal(playerHand);
+        if (playerTotal > 21) {
+            // Player busts
+            alert('Player Busts! Dealer Wins!');
+            // Reset the game
+            $('#start-btn').prop('disabled', false);
+            $('#hit-btn').prop('disabled', true);
+            $('#stand-btn').prop('disabled', true);
+        }
+    });
+
+    $('#stand-btn').click(function() {
+        $('#hit-btn').prop('disabled', true);
+        $('#stand-btn').prop('disabled', true);
+
+        playDealer();
+    });
+});
+
 });
