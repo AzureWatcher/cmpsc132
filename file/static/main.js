1f74caacb13dc2d8b0763bbdc9f27226d8562710
BigNoahW <149721935+BigNoahW@users.noreply.github.com>
2023-12-11 01:00:24 +0000

    balls

--- file/static/main.js
+++ file/static/main.js
@@ -16,6 +16,23 @@ function startGame() {
 
 // Function to get a new deck
 function getDeck() {
+    const suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
+    const ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace'];
+
+    const deck = [];
+
+    for (const suit of suits) {
+        for (const rank of ranks) {
+            const card = {
+                suit: suit,
+                rank: rank
+            };
+            deck.push(card);
+        }
+    }
+
+    return deck;
+}
     // Implement logic to generate a deck of cards
 }
 
